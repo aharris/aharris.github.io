dc324eae865c6c6ce1a7d6ca76ff47fa444c6ff1
Ashton Harris <ashtonharris.me@gmail.com>
2014-05-23 16:11:42 -0400

    Updates fonts and logo

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,31 @@
 (function(){
 
-  var app = angular.module('app', []);
+  var app = angular.module('app', ['ngRoute']);
 
   app.controller('AppController', function(){
-    this.title = "Ashton Harris";
+    // this.title = "Ashton Harris";
   });
+
+  app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/', {
+        redirectTo: '/work'
+      })
+      .when('/work', {
+        templateUrl : 'templates/work.html',
+        controller: 'AppController'
+      })
+      .when('/about', {
+        templateUrl : 'templates/about.html',
+        controller: 'AppController'
+      })
+      .when('/contact', {
+        templateUrl : 'templates/contact.html',
+        controller: 'AppController'
+      })
+      .otherwise({
+        redirectTo: 'templates/404.html'
+      });
+  }]);
 
 })();
