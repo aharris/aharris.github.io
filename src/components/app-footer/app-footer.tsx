78651fb364d2161b83d827490c91c99d33674944
Ashton Harris <ashtonharris.me@gmail.com>
2023-04-25 19:13:48 -0400

    Add social media

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,6 +1,8 @@
 import { Component, h } from '@stencil/core';
 
 import Mail from '../../assets/icons/envelope.svg';
+import Twitter from '../../assets/icons/twitter.svg';
+import Insta from '../../assets/icons/instagram.svg';
 
 
 @Component({
@@ -10,20 +12,35 @@ import Mail from '../../assets/icons/envelope.svg';
 })
 export class AppFooter {
   render() {
-    console.log('mail: ', Mail);
     return (
       <footer>
         <hr class="rhythm-8"/>
 
-        <div class="row">
-          <div class="col-xs">
+        <div class="row middle-xs">
+          <div class="col-xs profile-pic-col">
             <div class="profile-pic-wrap">
               <img class="profile-pic" src="/assets/images/about/ashton.png" alt="Photo of Ashton" />
             </div>
           </div>
+
           <div class="col-xs footer-line">
-            <div class='svg-container' innerHTML={Mail}></div>
-            <a href="mailto:ashton.harris@gmail.com">ashton.harris@gmail.com</a>
+            <ul>
+              <li class="footer-line">
+                <a class="subtitle footer-link" href="mailto:ashton.harris@gmail.com">
+                  <span class='svg-container' innerHTML={Mail}></span> ashton.harris@gmail.com
+                </a>
+              </li>
+              <li class="footer-line">
+                <a class="subtitle footer-link" href="https://twitter.com/busypixels" target='_blank'>
+                <span class='svg-container' innerHTML={Twitter}></span> @busypixels
+                </a>
+              </li>
+              <li class="footer-line">
+                <a class="subtitle footer-link" href="https://www.instagram.com/busypixels/" target='_blank'>
+                <span class='svg-container' innerHTML={Insta}></span> @busypixels
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </footer>
